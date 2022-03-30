25dd4f50d5466ce1e1b9c85520084c89057d34e6
Italo Castro <italocesarcastroo@gmail.com>
2022-03-30 11:53:32 -0300

    ConhecimentosComFonte

--- js/jsIndex.js
+++ js/jsIndex.js
@@ -1,3 +1,13 @@
 function enviarMensagemWtsApp() {
-    
+    x = document.getElementById("textoMensagemWtsApp");
+    mensagem = x.value;
+    window.open("https://api.whatsapp.com/send?phone=37998089731&text="+mensagem); 
+   
+}
+
+function enviarMensagemEmail() {
+    x = document.getElementById("textoMensagemEmail");
+    mensagem = x.value;
+    window.open("mailto:?subject=Portfólio&amp;body=Olá,%0D%0A%0D%0A" + mensagem + "%0D%0A%0D%0AAtenciosamente,%0D%0A[nome do usuário]"); 
+
 }
